665be1495630b276a9fa3663608a27f71447d470
Levon Avetisyan <avetisyan.levon@gmail.com>
2020-01-25 14:55:35 +0300

    adapter work from file read

--- index.js
+++ index.js
@@ -3,35 +3,46 @@ const fs = require('fs');
 const es = require('event-stream');
 
 
-
 const server = net.createServer(function (c) { //'connection' listener
     console.log('server connected');
+    //
+    // let lines = [];
+    //
+    // let s = fs.createReadStream('unit.txt')
+    //     .pipe(es.split())
+    //     .pipe(es.mapSync(function (line) {
+    //             //pause the readstream
+    //             s.pause();
+    //             lines.push(line);
+    //             s.resume();
+    //         })
+    //             .on('error', function (err) {
+    //                 console.log('Error:', err);
+    //             })
+    //             .on('end', function () {
+    //                 console.log('Finish file reading.');
+    //                 let index = 0;
+    //                 setInterval(() => {
+    //
+    //                     // const str = lines[index++].replace(/\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{6}/, '');
+    //                     const date = new Date().toISOString();
+    //                     const str = date + '|' + lines[index++] + '\n';
+    //                     console.log(str)
+    //                     c.write(str)
+    //                 }, 1000)
+    //
+    //
+    //             })
+    //     );
 
-    let lines = [];
-
-    let s = fs.createReadStream('text.txt')
-        .pipe(es.split())
-        .pipe(es.mapSync(function (line) {
-                //pause the readstream
-                s.pause();
-                lines.push(line);
-                s.resume();
-            })
-                .on('error', function (err) {
-                    console.log('Error:', err);
-                })
-                .on('end', function () {
-                    console.log('Finish file reading.');
-                    let index = 0;
-                    setInterval(() => {
-
-                        // const str = lines[index++].replace(/\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}\.\d{6}/, '');
-                        const str = lines[index++] + '\n';
-                        console.log(str)
-                        c.write(str)
-                    }, 10)
-                })
-        );
+    setInterval(() => {
+        fs.readFile('./unit.txt', 'utf8', (err, data) => {
+            const date = new Date().toISOString();
+            const str = date + '|' + data + '\n';
+            console.log(str);
+            c.write(str)
+        });
+    }, 1000)
 
     c.on('end', function () {
         console.log('server disconnected');
